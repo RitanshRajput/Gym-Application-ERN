0921d3208f99e002d213421c9a74c61de9e5861c
Ritansh Singh <90311141+RitanshRajput@users.noreply.github.com>
2024-05-31 16:55:57 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ config({ path: "./config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: ["https://gym-application-ern-frontend.vercel.app"],
     methods: ["POST"],
     credentials: true,
   })
